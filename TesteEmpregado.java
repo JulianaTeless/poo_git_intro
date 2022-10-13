7a35ff21a2a30f31722009de63e305f2dfbd9a1d
Juliana Teles <jtelessoares.jt@gmail.com>
2022-10-13 14:28:49 -0300

    Primeiro teste concluido com sucesso

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -3,5 +3,20 @@ public class TesteEmpregado {
         Empregado Mateus = new Empregado();
         Empregado Maria = new Empregado();
         Empregado Jose = new Empregado();
+
+        Mateus.setTipo(1);
+        Maria.setTipo(2);
+        Jose.setTipo(3);
+
+        Mateus.setSalario(2000);
+        Maria.setSalario(1700);
+        Jose.setSalario(1500);
+
+        Maria.setComissao(0.2);
+        Jose.setBonus(400);
+
+        System.out.println(Mateus.calculaSalario());
+        System.out.println(Maria.calculaSalario());
+        System.out.println(Jose.calculaSalario());
     }
 }
